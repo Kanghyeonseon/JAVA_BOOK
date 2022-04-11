44638502ea571aeba8f968d98fe3140623f81868
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-04-11 17:38:30 +0900

    C5

--- src/Ch04/C01Scanner.java
+++ src/Ch04/C01Scanner.java
@@ -17,8 +17,11 @@ public class C01Scanner {
 		//위치정보를 sc에 넣겠다.     //스캐너에 표준입력스트림을 연결하겠다.
 		
 		
+		//new Scanner(System.in)
+		//->키보드와 
+		System.out.print("정수 3개 입력 : ");
 		int num1 = sc.nextInt(); //키보드로 무언가를 입력하면 정수값 하나를 받아와서 num1에 넣어준다.
-		int num2 = sc.nextInt();
+		int num2 = sc.nextInt(); //띄어쓰기 or 엔터를 기준으로 다음 값을 입력한다.
 		int num3 = sc.nextInt();
 		int sum = num1 + num2 + num3;
 		System.out.println("Sum : " + sum);
