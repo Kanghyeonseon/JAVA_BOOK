1790a7d4ffe782e0485c4493522114aff5d55482
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-04-15 09:54:13 +0900

    C2

--- src/Ch08/For/C01HelloWorld.java
+++ src/Ch08/For/C01HelloWorld.java
@@ -4,16 +4,22 @@ public class C01HelloWorld {
 
 	public static void main(String[] args) {
 
-		int i = 1; //탈출용 변수
-		while(i<=10) //탈출 조건식
+		/*-------------------------------------------- 
+		 * 이 부분을 for문으로 옮겼다.
+		int i = 1; 		//탈출용 변수
+		while(i<=10) 	//탈출 조건식
 		{
 			System.out.println("Hello World");
-			i++; //탈출 연산식
+			i++; 		//탈출 연산식
 
 		}
+		--------------------------------------------*/
 		
-		
-		
+		for(int i=1; i<=10; i++) {
+		//탈출용변수 //탈출조건식 //탈출연산식
+			System.out.println("Hello World");
+
+		}
 	}
 
 }
