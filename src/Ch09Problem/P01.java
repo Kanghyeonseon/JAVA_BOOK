b5bf3dd4397c5f9d031375f5cadb7021c2edc832
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-04-18 15:49:54 +0900

    C3

---
+++ src/Ch09Problem/P01.java
@@ -0,0 +1,39 @@
+/*자바 클래스를 작성하는 연습을 해보자. 
+1) 
+public static void main(String[] args) {
+   Person Jung = new Person("홍길동", 44, "대구"); //이름(name),나이(age),주소(addr)
+   Jung.showinfo();
+}
+[출력]
+이름 : 홍길동
+나이 : 44
+주소 : 대구
+*/
+package Ch09Problem;
+class Person {
+	String name;
+	int age;
+	String addr;
+	
+	Person(String name, int age, String addr) {
+		this.name = name; this.age = age; this.addr = addr;
+	}
+	
+
+	void showinfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("주소 : " + addr);
+	}
+
+}
+
+
+public class P01 {
+
+	public static void main(String[] args) {
+		   Person Jung = new Person("홍길동", 44, "대구"); //이름(name),나이(age),주소(addr)
+		   Jung.showinfo();
+		}
+
+}
---
+++ src/Ch09Problem/P02.java
@@ -0,0 +1,46 @@
+/*----------------------------
+2.
+----------------------------
+--Position.java--
+class Position{
+
+}
+--Main2.java--
+public static void main(String[] args) {
+   Position pos1 = new Position(10); //x좌표(int x)
+   Position pos2 = new Position(30,40); //x좌표(int x),y좌표(int y)
+   pos1.showpos();
+   pos2.showpos();
+}
+[출력]
+X : 10 , Y : 0
+X : 30 , Y : 40
+
+*/
+package Ch09Problem;
+class Position{
+	int x; int y;
+	
+	
+	Position(int x, int y) {
+		this.x=x;this.y=y;
+	}
+	Position(int x) {
+		this.x=x;
+	}
+	
+	void showpos() {
+		System.out.printf("X : %d , Y : %d\n",x,y);
+	}
+	
+	
+}
+
+public class P02 {
+	public static void main(String[] args) {
+		   Position pos1 = new Position(10); //x좌표(int x)
+		   Position pos2 = new Position(30,40); //x좌표(int x),y좌표(int y)
+		   pos1.showpos();
+		   pos2.showpos();
+		}	
+}
