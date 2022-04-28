d1d89b110f3f33aa75937291905858d101eb909d
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-04-28 09:35:02 +0900

    C1

---
+++ src/Ch35/C01SwingMain.java
@@ -0,0 +1,9 @@
+package Ch35;
+import javax.swing.JFrame;
+public class C01SwingMain {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("첫번째 프레임입니다."); //창제목
+		frame.setBounds(100,100,700,700); //
+	}
+}
